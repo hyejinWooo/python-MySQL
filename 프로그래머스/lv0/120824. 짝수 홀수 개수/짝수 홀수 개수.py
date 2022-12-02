d780992d4d27d79513e3d68514e3d3c5bd76055f
hyejinWooo <114497088+hyejinWooo@users.noreply.github.com>
2022-12-02 17:18:07 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -0,0 +1,11 @@
+def solution(num_list):
+    answer = []
+    odd = 0
+    even = 0
+    for i in num_list:
+        if i%2 == 0:
+            even = even+1
+        else:
+            odd = odd+1
+    answer = [even, odd]
+    return answer

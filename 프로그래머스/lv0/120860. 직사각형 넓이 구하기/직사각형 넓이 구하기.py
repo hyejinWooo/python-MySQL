c1c9bf02c1986928923e798a306be4c550b88484
hyejinWooo <114497088+hyejinWooo@users.noreply.github.com>
2023-02-16 16:39:25 +0900

    [level 0] Title: 직사각형 넓이 구하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.py
+++ 프로그래머스/lv0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.py
@@ -1,8 +1,11 @@
-def solution(dots):
-    x = [dot[0] for dot in dots]
-    y = [dot[1] for dot in dots]
+# def solution(dots):
+#     x = [dot[0] for dot in dots]
+#     y = [dot[1] for dot in dots]
     
-    w = max(x) - min(x)
-    h = max(y) - min(y)
-    area = w*h
-    return area
+#     w = max(x) - min(x)
+#     h = max(y) - min(y)
+#     area = w*h
+#     return area
+
+def solution(dots):
+    return (max(dots)[0] - min(dots)[0])*(max(dots)[1] - min(dots)[1])

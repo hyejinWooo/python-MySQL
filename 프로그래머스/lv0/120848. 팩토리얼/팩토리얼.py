c2c282970af239d0b47174f0d39e0dc61016bb8b
hyejinWooo <114497088+hyejinWooo@users.noreply.github.com>
2023-03-07 18:30:51 +0900

    [level 0] Title: 팩토리얼, Time: 0.01 ms, Memory: 9.98 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.py
@@ -0,0 +1,16 @@
+# import math
+# def solution(n):
+#     answer = 0
+#     for i in range(1, n):
+#         if math.factorial(i) > n:
+#             break
+#     return i-1
+
+import math
+
+def solution(n):
+    answer = 0
+    for i in range(1, 12):
+        if n < math.factorial(i):
+            return i - 1
+    return answer

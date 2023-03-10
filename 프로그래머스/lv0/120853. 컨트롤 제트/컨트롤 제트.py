95b8134404385ddf6134bad25c105578ab13a4c1
hyejinWooo <114497088+hyejinWooo@users.noreply.github.com>
2023-03-10 16:04:33 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.py
+++ 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.py
@@ -1,7 +1,6 @@
 def solution(s):
     answer = []
     s = s.split(' ')
-    # s = s.replace(" ", "")
     for i in range(0, len(s)):
         if s[i] != 'Z':
             answer.append(s[i])
@@ -10,6 +9,16 @@ def solution(s):
     answer = [int(i) for i in answer]
     answer = sum(answer)
     return answer
+
+# def solution(s):
+#     arr = s.split(' ')
+#     result =[]
+#     for i in arr :
+#         if i=='Z':
+#             result.pop()
+#         else:
+#             result.append(int(i))
+#     return sum(result)
 
 # def solution(s):
 #     answer = 0

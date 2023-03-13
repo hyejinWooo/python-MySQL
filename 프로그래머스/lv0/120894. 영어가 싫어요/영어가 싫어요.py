3d7ab47241149c1a0bc7003892ffedc50419429f
hyejinWooo <114497088+hyejinWooo@users.noreply.github.com>
2023-03-13 16:17:56 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.py
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.py
@@ -1,23 +1,16 @@
-# def solution(numbers):
-#     answer1 = []
-#     answer2 = ''
-#     num = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-#     numdict = {"zero":'0', "one":'1', "two":'2', "three":'3', "four":'4', "five":'5', "six":'6', "seven":'7', "eight":'8', "nine":'9'}
-#     idx = []
-#     for i in num:
-#         if i in numbers:
-#             idx.append(numbers.find(i))
-#             idx.sort()
-#     for i in range(0, len(idx)-1):
-#         answer1.append(numbers[idx[i]:idx[i+1]])
-#     answer1.append(numbers[idx[len(idx)-1]:])
-#     for i in answer1:
-#         answer2 = answer2 + numdict[i]
-#     return int(answer2)
-
 def solution(numbers):
-    numdict = {'zero' : 0, 'one':1, 'two':2, 'three' : 3, 'four':4, 
-                 'five':5,'six' : 6, 'seven' : 7, 'eight':8,'nine':9}
-    for k, v in numdict.items():
-        numbers = numbers.replace(k, str(v))
-    return int(numbers)
+    answer1 = []
+    answer2 = ''
+    num = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    numdict = {"zero":'0', "one":'1', "two":'2', "three":'3', "four":'4', "five":'5', "six":'6', "seven":'7', "eight":'8', "nine":'9'}
+    idx = []
+    for i in num:
+        if i in numbers:
+            idx.append(numbers.find(i))
+            idx.sort()
+    for i in range(0, len(idx)-1):
+        answer1.append(numbers[idx[i]:idx[i+1]])
+    answer1.append(numbers[idx[len(idx)-1]:])
+    for i in answer1:
+        answer2 = answer2 + numdict[i]
+    return int(answer2)

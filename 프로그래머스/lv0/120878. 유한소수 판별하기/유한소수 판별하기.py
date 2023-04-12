4c35523564acb330407369c3144b74263240b12d
hyejinWooo <114497088+hyejinWooo@users.noreply.github.com>
2023-04-12 17:06:08 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.py
@@ -0,0 +1,33 @@
+# import math
+# def solution(a, b):
+#     answer = 0
+#     gcd = math.gcd(a, b)
+#     b = b / gcd
+    
+#     num = []
+#     i = 2
+#     while i <= b:
+#         if b % i == 0:
+#             num.append(i)
+#         else:
+#             i += 1
+#     return num
+
+import math
+
+def solution(a, b):
+    gcd = math.gcd(a, b) # 분모, 분자의 최대공약수를 구해 b를 최대공약수로 나눔
+    b //= gcd
+    
+    num = [] # 소인수 저장
+    i = 2
+    while i <= b: # 소인수 구하기
+        if b % i == 0:
+            b //= i
+            num.append(i)
+        else:
+            i += 1
+    
+    if all(i in [2,5] for i in num): # 소인수가 2와 5만 존재하면 유한소수 그렇징 않으면 무한소수
+        return 1
+    return 2
